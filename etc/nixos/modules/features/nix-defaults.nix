39cf3b8939a62e201d4ef1e050a45a52628674cd
Lucas Fernando Cardoso Nunes <lucasfc.nunes@gmail.com>
2026-05-05 16:10:34 -0300

    build(nixos): improve caching

--- etc/nixos/modules/features/nix-defaults.nix
+++ etc/nixos/modules/features/nix-defaults.nix
@@ -21,21 +21,22 @@
             "nix-command"
             "flakes"
           ];
-          # Cache
           connect-timeout = 5;
           fallback = true;
           accept-flake-config = true;
           substituters = [
-            # "https://cache.nixos.org"
+            "https://cache.nixos.org?priority=10"
+            "https://nix-mirror.freetls.fastly.net?priority=10"
+          ];
+          trusted-substituters = [
             "https://nix-community.cachix.org?priority=41"
             "https://numtide.cachix.org?priority=42"
-            # "https://nix-mirror.freetls.fastly.net"
           ];
           trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
           ];
-          # Download optimization
           http-connections = 128;
           max-substitution-jobs = 128;
           max-jobs = "auto";
