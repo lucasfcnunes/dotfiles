15663f7f73cc802cb74281da12b8dc48cb99a94e
Lucas Fernando Cardoso Nunes <lucasfc.nunes@gmail.com>
2026-09-21 09:31:42 -0300

    feat: jj, git and signing config

--- etc/nixos/modules/features/lucasfcnunes-hm.nix
+++ etc/nixos/modules/features/lucasfcnunes-hm.nix
@@ -76,6 +76,12 @@
       pkgs,
       ...
     }:
+    let
+      my-name = "Lucas Fernando Cardoso Nunes";
+      my-email = "lucasfc.nunes@gmail.com";
+      my-gpg = "51BBB719DB9A3F8A928A4D5C9D76B9D8E1E20E99";
+      my-ssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlBdpRehqTLYT2XKNJndv0pMWCBuqdLUPCDc1xd2XLp (gpg-ssh-lucasfcnunes)";
+    in
     {
       imports = [
         self.homeModules.npiperelay-wsl
@@ -106,7 +112,7 @@
           # INFO: https://home-manager-options.extranix.com/?query=programs.gpg&release=release-25.11
           enable = true;
           settings = {
-            # default-key = "51BBB719DB9A3F8A928A4D5C9D76B9D8E1E20E99";
+            # default-key = my-gpg;
             # personal-digest-preferences = "SHA512";
             # cert-digest-algo = "SHA512";
             # default-preference-list = "SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed";
@@ -119,7 +125,7 @@
           publicKeys = [
             {
               source = pkgs.fetchurl {
-                url = "https://keybase.io/lucasfcnunes/pgp_keys.asc?fingerprint=51bbb719db9a3f8a928a4d5c9d76b9d8e1e20e99";
+                url = "https://keybase.io/lucasfcnunes/pgp_keys.asc?fingerprint=${lib.toLower my-gpg}";
                 sha256 = "93f1beb2d91b47b78052161316da25169c3920c843974786aea46dba458e89b5";
                 # url = "https://github.com/lucasfcnunes.gpg";
                 # sha256 = "a12def83439c20a6a36b082a9169596965807f042b2848202075702798cf80b4"; # only trailing newline changed
@@ -179,6 +185,8 @@
                 # PATH="$PATH:/opt/bin:$HOME/.local/bin/"
 
                 [[ ! -f ~/.dotbins/shell/zsh.sh ]] || source ~/.dotbins/shell/zsh.sh
+
+                # eval "$(devenv hook zsh)" # ! using direnv for now
               '';
             in
             lib.mkMerge [
@@ -189,13 +197,15 @@
         git = {
           enable = true;
           settings = {
+            # INFO: https://git-scm.com/docs/git-config
             user = {
-              name = "Lucas Fernando Cardoso Nunes";
-              email = "lucasfc.nunes@gmail.com";
+              name = my-name;
+              email = my-email;
             };
-            # gpg.format = "ssh";
-            # user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlBdpRehqTLYT2XKNJndv0pMWCBuqdLUPCDc1xd2XLp (gpg-ssh-lucasfcnunes)";
-            user.signingKey = "51BBB719DB9A3F8A928A4D5C9D76B9D8E1E20E99";
+            gpg.format = "ssh";
+            user.signingKey = my-ssh;
+            # gpg.format = "gpg";
+            # user.signingKey = my-gpg;
             commit.gpgSign = true;
             tag.gpgSign = true;
             tag.forceSignAnnotated = true;
@@ -216,8 +226,29 @@
             # core = {
             #   editor = "nvim";
             # };
+            log = {
+              abbrevCommit = true;
+              showSignature = true;
+            };
           };
         };
+        jujutsu = {
+          enable = true;
+          settings = {
+            user = {
+              name = my-name;
+              email = my-email;
+            };
+            signing = {
+              behavior = "own";
+              # backend = "gpg";
+              # key = my-gpg;
+              backend = "ssh";
+              key = my-ssh;
+            };
+          };
+        };
+        jjui.enable = true;
         gh = {
           enable = true;
           # settings.editor = "nvim";
